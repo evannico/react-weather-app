2ae18f02f2286649bc2432ef029e943902f8a01f
Evan Albert <evan@cyberdust.com>
2017-02-28 11:18:22 -0800

    added content and styling to About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,20 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>welcome to the about page</p>
+      <h3 className="text-center">About</h3>
+      <p>This is a weather application built on React.</p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This is the JS framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+        <li>
+          <a href="http://foundation.zurb.com/">Foundation</a> - This is the Front-End framework used.
+        </li>
+      </ul>
     </div>
   )
 };
