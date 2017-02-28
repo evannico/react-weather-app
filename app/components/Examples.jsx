1cd184dff578217b90947579ae24d6a1c5828961
Evan Albert <evan@cyberdust.com>
2017-02-28 15:06:25 -0800

    wired up the search in nav and examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,10 +8,10 @@ var Examples = (props) => {
       <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=LosAngeles'>Los Angeles, CA</Link>
+          <Link to='/?location=Los%20Angeles'>Los Angeles, CA</Link>
         </li>
         <li>
-          <Link to='/?location=SanFrancisco'>San Francisco, CA</Link>
+          <Link to='/?location=San%20Francisco'>San Francisco, CA</Link>
         </li>
         <li>
           <Link to='/?location=Miami'>Miami, FL</Link>
